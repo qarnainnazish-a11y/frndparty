436b849aba948f579aa4c981e0e811dceacea086
qarnainnazish-a11y <qarnainnazish@gmail.com>
2026-03-02 19:17:00 +0530

    Create firebase-config.js

---
+++ firebase-config.js
@@ -0,0 +1,9 @@
+export const firebaseConfig = {
+  apiKey: "CHANGE_ME",
+  authDomain: "CHANGE_ME",
+  databaseURL: "CHANGE_ME",
+  projectId: "CHANGE_ME",
+  storageBucket: "CHANGE_ME",
+  messagingSenderId: "CHANGE_ME",
+  appId: "CHANGE_ME"
+};
