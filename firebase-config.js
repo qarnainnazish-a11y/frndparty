80750ddd430724783fd52956506abc6774c482d5
qarnainnazish-a11y <qarnainnazish@gmail.com>
2026-03-02 19:35:47 +0530

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,9 +1,8 @@
 export const firebaseConfig = {
-  apiKey: "CHANGE_ME",
-  authDomain: "CHANGE_ME",
-  databaseURL: "CHANGE_ME",
-  projectId: "CHANGE_ME",
-  storageBucket: "CHANGE_ME",
-  messagingSenderId: "CHANGE_ME",
-  appId: "CHANGE_ME"
+  apiKey: "AIzaSyAc9WLJso4c4K-igEpk51nTzTWACfMZTkQ",
+  authDomain: "frndparty-ab2e9.firebaseapp.com",
+  projectId: "frndparty-ab2e9",
+  storageBucket: "frndparty-ab2e9.firebasestorage.app",
+  messagingSenderId: "451894219901",
+  appId: "1:451894219901:web:d161db8f21bc91a9c68d14"
 };
